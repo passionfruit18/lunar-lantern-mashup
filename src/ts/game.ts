fd2859f4cbf65d4a0a3ed975058098246ac9f809
James Dai <james.dai.uk@googlemail.com>
2026-02-21 12:14:31 -0500

    (refactor): Break player-list out of game.ts

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -4,6 +4,7 @@ declare var io: any;
 import * as BoardModule from "./board";
 import * as PlayerModule from "./player";
 import './lunar-background';
+import './player-list'
 import { socket, toggleLoaders } from "./util";
 import { getHint, makeHintScrollDraggable } from "./hint";
 import { gameState } from "./game-state";
@@ -264,22 +265,6 @@ function prepareCanvas(canvas: HTMLCanvasElement) {
 }
 
 
-// This fires for everyone whenever the player list changes
-socket.on('player_list_updated', (data: {'players': PlayerModule.PlayerData[]}) => {
-    updatePlayerSidebar(data.players);
-});
-
-function updatePlayerSidebar(players: PlayerModule.PlayerData[]) {
-    const listElement = document.getElementById('player-list') as HTMLElement;
-    listElement.innerHTML = ""; // Clear current list
-    players.forEach(player => {
-        const li = document.createElement('li');
-        li.innerHTML = PlayerModule.printAllPlayerDetailsPretty(player);
-        listElement.appendChild(li);
-    });
-}
-
-
 function leaveGame() {
     location.reload();
 }
@@ -313,23 +298,6 @@ function triggerExplosion() {
     }
 }
 
-
-// Define your initialization function
-function initializeUI() {
-
-    makeHintScrollDraggable()
-    setupUIListenersInput()
-    setupUIListenersButtons()
-}
-
-
-// The "Document Ready" Listener
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initializeUI);
-} else {
-    // DOM is already ready (e.g., if the script is deferred or loaded late)
-    initializeUI();
-}
 
 function setupUIListenersInput() {
 
@@ -375,3 +343,17 @@ function setupUIListenersButtons() {
     const hintBtn = document.getElementById('hint-btn')?.addEventListener('click', getHint);    
 }
 
+// Define your initialization function
+function initializeUI() {
+    makeHintScrollDraggable()
+    setupUIListenersInput()
+    setupUIListenersButtons()
+}
+
+// The "Document Ready" Listener
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', initializeUI);
+} else {
+    // DOM is already ready (e.g., if the script is deferred or loaded late)
+    initializeUI();
+}
